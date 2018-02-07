a19e0e1ceeb7c9534f293d6264acf6c0240aa07b
Edison Neto <ednetoali@gmail.com>
2018-02-06 22:52:17 -0200

    Now I can change brigthness inline

--- brigthness.sh
+++ brigthness.sh
@@ -2,30 +2,30 @@
 
 # This script helps me change the brigthness of my computer screen.
 
-echo "Select:"
-echo "----------------------"
-echo "|1| Night mode"
-echo "|2| Not so bright mode"
-echo "|3| Bright mode"
-echo "|4| Too bright mode"
-echo "|5| I can't see mode"
-echo "----------------------"
+#echo "Select:"
+#echo "----------------------"
+#echo "|1| Night mode"
+#echo "|2| Not so bright mode"
+#echo "|3| Bright mode"
+#echo "|4| Too bright mode"
+#echo "|5| I can't see mode"
+#echo "----------------------"
 
-read option
+#read option
 
-if [ "$option" -eq 1 ]
+if [ "$1" -eq 1 ]
 then
 	xbacklight -set 10
-elif [ "$option" -eq 2 ]
+elif [ "$1" -eq 2 ]
 then
 	xbacklight -set 25
-elif [ "$option" -eq 3 ]
+elif [ "$1" -eq 3 ]
 then
 	xbacklight -set 50
-elif [ "$option" -eq 4 ]
+elif [ "$1" -eq 4 ]
 then
 	xbacklight -set 75
-elif [ "$option" -eq 5 ]
+elif [ "$1" -eq 5 ]
 then
 	xbacklight -set 100
 fi
