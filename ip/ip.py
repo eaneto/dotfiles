e9c2baf725ea5c54258e5de080cef9154d86e3a2
Edison Neto <ednetoali@gmail.com>
2018-05-12 22:34:57 -0300

    modified:   ip/ip.py
    Just updated the code to get my ip. Removed unused import of
    beatifulsoup4 and modified the exception.

--- ip/ip.py
+++ ip/ip.py
@@ -1,11 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
+
 
 def main():
     try:
         r = requests.get('https://wtfismyip.com/json')
         data = r.json()
-    except:
+    except Exception as exp:
         print("\nCan't connect to the server.\n")
         return False
 
@@ -16,6 +16,7 @@ def main():
     print("\nYour IP Adress is: {ip}".format(ip=ip))
     print("\nYour ISP is: {isp}".format(isp=isp))
     print("\nYour Location is: {location}".format(location=location))
+
 
 if __name__ == '__main__':
     main()
