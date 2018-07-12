ffc5ee2b19aa024a47668cf119da8990909a3861
Edison Neto <ednetoali@gmail.com>
2018-07-12 17:33:52 -0300

    Added a way to check if the file is a directory
    pull_everything.sh

--- pull_everything.sh
+++ pull_everything.sh
@@ -4,10 +4,14 @@ export PROJECTS_DIR=/home/eldron/Projects/*
 
 for directory in $PROJECTS_DIR
 do
-    cd $directory
-    is_repo=$(git status | grep -i "branch" | wc -l)
-    if [ $is_repo != 0 ];
+    is_dir=$(file $entry | grep directory | wc -l)
+    if [ is_dir = 1 ];
     then
-        git status && git pull
+        cd $directory
+        is_repo=$(git status | grep -i "branch" | wc -l)
+        if [ $is_repo != 0 ];
+        then
+            git status && git pull
+        fi
     fi
 done
