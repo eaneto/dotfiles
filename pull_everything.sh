339dab076a8015bf7de8bd8252cf7570afb8b972
Edison Neto <ednetoali@gmail.com>
2018-07-13 15:20:35 -0300

    modified:   pull_everything.sh 	modified:   rhythmbox.sh
    Updated couple of scripts.

--- pull_everything.sh
+++ pull_everything.sh
@@ -4,8 +4,8 @@ export PROJECTS_DIR=/home/eldron/Projects/*
 
 for directory in $PROJECTS_DIR
 do
-    is_dir=$(file $entry | grep directory | wc -l)
-    if [ is_dir = 1 ];
+    is_dir=$(file $directory | grep directory | wc -l)
+    if [ is_dir != 0 ];
     then
         cd $directory
         is_repo=$(git status | grep -i "branch" | wc -l)
--- rhythmbox.sh
+++ rhythmbox.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-rhythmbox-client --print-playing-format="%tt - %te/%td"
+is_opened=$(ps -ef | grep rhythmbox | wc -l)
+if [ "$is_opened" -ne 0 ];
+then
+    rhythmbox-client --print-playing-format="%tt - %te/%td"
+else
+    echo "No song"
+fi
