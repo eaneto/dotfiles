dae2286e3d47f59c6a782eaa03646d19e28199da
Edison Neto <ednetoali@gmail.com>
2018-07-31 15:08:41 -0300

    Adds a gitignore and a script to get dolar in real

---
+++ dolar.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.dolarhoje.net.br")
+data = BeautifulSoup(r.text, 'html.parser')
+
+print("R${}".format(data.find(id="moeda")["value"]))
