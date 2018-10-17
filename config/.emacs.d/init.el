cf4c83ab3080de93d1ac4d58326bf10bea59d48e
Edison Neto <ednetoali@gmail.com>
2018-10-17 17:33:13 -0300

    Update emacs config

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -1,19 +1,10 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(jdee-server-dir "/home/eldron/jdee-server")
  '(package-selected-packages
    (quote
-	(highlight-indent-guides telephone-line yaml-mode dockerfile-mode company-anaconda anaconda-mode pyenv-mode company-arduino all-the-icons neotree gh-md sphinx-doc arduino-mode evil-magit async magit flycheck jdee latex-preview-pane function-args java-snippets react-snippets ## python-doctring web-mode markdown-mode php-mode rjsx-mode js2-mode evil py-autopep8 goto-chg undo-tree auto-complete elpy base16-theme which-key try use-package)))
+	(color-identifiers-mode highlight-indent-guides telephone-line yaml-mode dockerfile-mode company-anaconda anaconda-mode pyenv-mode company-arduino all-the-icons neotree gh-md sphinx-doc arduino-mode evil-magit async magit flycheck jdee latex-preview-pane function-args java-snippets react-snippets ## python-doctring web-mode markdown-mode php-mode rjsx-mode js2-mode evil py-autopep8 goto-chg undo-tree auto-complete elpy base16-theme which-key try use-package)))
  '(standard-indent 4))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
 
 (load "~/.emacs.d/scratch.el")
 (load "~/.emacs.d/packages.el")
--- config/.emacs.d/packages.el
+++ config/.emacs.d/packages.el
@@ -1,5 +1,14 @@
+;;                  _                         
+;; _ __   __ _  ___| | ____ _  __ _  ___  ___ 
+;;| '_ \ / _` |/ __| |/ / _` |/ _` |/ _ \/ __|
+;;| |_) | (_| | (__|   < (_| | (_| |  __/\__ \
+;;| .__/ \__,_|\___|_|\_\__,_|\__, |\___||___/
+;;|_|                         |___/
+;;
+
 (require 'package)
 (setq package-enable-at-startup nil)
+
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
@@ -17,12 +26,24 @@
 (setq elpy-rpc-python-command "python3")
 (setq python-shell-interpreter "python3")
 
+(use-package py-autopep8
+  :ensure t)
+
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+(require 'anaconda-mode)
+(require 'pyenv-mode)
+
+(use-package company
+  :ensure t)
 (eval-after-load "company"
  '(add-to-list 'company-backends 'company-anaconda))
 (add-hook 'python-mode-hook 'anaconda-mode)
 
 (use-package try
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package which-key
   :ensure t
@@ -33,38 +54,29 @@
   :config
   (load-theme 'base16-tomorrow-night t))
 
-(use-package auto-complete
-  :ensure t)
-
-(ac-config-default)
-
-(use-package py-autopep8
-  :ensure t)
-
-(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
 (use-package magit
-  :ensure t)
+  :ensure t
+  :defer t)
 
 ;; Evil mode.
 (require 'evil)
 (evil-mode 1)
 
 (use-package evil-magit
-  :ensure t)
+  :ensure t
+  :config (setq evil-magit-state 'motion))
 
-(setq evil-magit-state 'motion)
 (require 'evil-magit)
 
 (use-package js2-mode
   :ensure t)
 
 (use-package rjsx-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist
+	     '("components\\/.*\\.js\\'" . rjsx-mode)))
 
-(add-to-list 'auto-mode-alist
-	     '("components\\/.*\\.js\\'" . rjsx-mode))
 
 (require 'php-mode)
 
@@ -78,9 +90,8 @@
   :ensure t)
 
 (use-package function-args
-  :ensure t)
-
-(fa-config-default)
+  :ensure t
+  :init (fa-config-default))
 
 (use-package arduino-mode
   :ensure t)
@@ -88,9 +99,6 @@
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-
-(require 'anaconda-mode)
-(require 'pyenv-mode)
 
 (require 'all-the-icons)
 
@@ -100,6 +108,8 @@
 (latex-preview-pane-enable)
 
 ;; Regular auto-complete initialization
+(use-package auto-complete
+  :ensure t)
 (require 'auto-complete-config)
 (ac-config-default)
 
