696eea974c27609244aef6abb065bbeebca2afe6
Edison Neto <ednetoali@gmail.com>
2018-10-24 13:54:11 -0300

    Update scripts

--- dolar.py
+++ dolar.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.dolarhoje.net.br")
 data = BeautifulSoup(r.text, 'html.parser')
 
-print("R${}".format(data.find(id="moeda")["value"]))
+print("R$ {}".format(data.find(id="moeda")["value"]))
--- yen.py
+++ yen.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.dolarhoje.com/iene/")
 data = BeautifulSoup(r.text, 'html.parser')
 
-print("¥{}".format(data.find(id="estrangeiro")["value"]))
+print("R$ {}".format(data.find(id="estrangeiro")["value"]))
