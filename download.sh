314cb40f19643b4fc401e713cc6b403f2d00be30
Edison Neto <ednetoali@gmail.com>
2018-11-07 13:22:52 -0200

    Adds option to not stop downloading on error

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ current_directory=$(pwd)
 
 if [ "$current_directory" = $HOME/Music ];
 then
-    youtube-dl "$1" -x --audio-format vorbis
+    youtube-dl "$1" -i -x --audio-format vorbis
 else
-    cd $HOME/Music && youtube-dl "$1" -x --audio-format vorbis
+    cd $HOME/Music && youtube-dl "$1" -i -x --audio-format vorbis
 fi
