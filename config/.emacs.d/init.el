3330e00097ca67ace7b20230f226fa06bd1fce38
Edison Neto <ednetoali@gmail.com>
2018-11-21 11:18:15 -0200

    Update emacs config
    Add base16 tomorrow-night theme in custom-set-variables, and update an
    alias for eshell.

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -5,10 +5,11 @@
 ;;; Code:
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"])
+ '(ansi-term-color-vector
+   [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"] t)
+ '(custom-enabled-themes (quote (base16-tomorrow-night)))
  '(package-selected-packages
    (quote
 	(apache-mode elixir-mode vue-mode mmm-mode python-docstring popwin eshell-prompt-extras color-identifiers-mode highlight-indent-guides telephone-line yaml-mode dockerfile-mode company-anaconda anaconda-mode pyenv-mode all-the-icons neotree gh-md sphinx-doc arduino-mode evil-magit async magit flycheck latex-preview-pane function-args ## python-doctring web-mode markdown-mode php-mode rjsx-mode js2-mode evil py-autopep8 goto-chg undo-tree auto-complete elpy base16-theme which-key use-package)))
@@ -20,11 +21,6 @@
 (load "~/.emacs.d/linux-style.el")
 (load "~/.emacs.d/customizations.el")
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
 
 ;;; init.el ends here
