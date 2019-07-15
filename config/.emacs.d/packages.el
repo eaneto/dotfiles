5c8d5dd21e86792131d86fa5544aa13aecac2e37
Edison Neto <ednetoali@gmail.com>
2019-07-15 20:41:02 -0300

    Update emacs dashboard config

--- config/.emacs.d/packages.el
+++ config/.emacs.d/packages.el
@@ -112,11 +112,11 @@
   :ensure t
   :config
   (dashboard-setup-startup-hook)
-  (setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5)
-                        (agenda . 5)
-                        (registers . 5))))
+  (setq dashboard-center-content t)
+  (setq dashboard-items '((recents . 5)
+                          (bookmarks . 5)
+                          (projects . 15)
+                          (agenda . 5))))
 
 (use-package sly
   :ensure t
