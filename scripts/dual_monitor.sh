6c6a764454f18ac8ed3e6e98973d779e01e46345
Edison Neto <ednetoali@gmail.com>
2019-07-25 18:47:59 -0300

    Moves evertything

--- scripts/dual_monitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-monitor=$(xrandr | grep -w "connected" | wc -l)
-
-if [ $monitor = 2  ];
-then
-    xrandr --output HDMI-1 --right-of eDP-1 --auto    
-fi
--- scripts/mack_net.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Making this script because the connection on my university
-# keeps going down on my computer, and i don't know why.
-
-nmcli c up CAMPUS_MACK --ask
