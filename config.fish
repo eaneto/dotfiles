e66572de6cabbad814018c8afa88c67f81a5b51d
Edison Neto <ednetoali@gmail.com>
2020-11-14 22:45:04 -0300

    Update fish config

--- config.fish
+++ config.fish
@@ -1,14 +1,17 @@
 #!/usr/bin/fish
 
-export SCRIPTS=$HOME/Projects/scripts-and-config/scripts
+# ENV variables
+export PROJECTS=$HOME/projects
+export EDITOR=emacs
+export GIT_EDITOR=vim
+export TERM=st
+export SCRIPTS=$PROJECTS/snippets/shell/scripts
 
 # Shortcuts
 alias v="vim"
 alias vi="vim"
 alias vimrc="vi $HOME/.vimrc"
 alias r="ranger"
-alias gpg="gpg2"
-alias pip3="/usr/local/bin/pip3"
 
 alias ls="ls -N --color"
 abbr ll "ls -laho"
@@ -16,23 +19,9 @@ abbr l "ls -l"
 abbr sudo "sudo"
 
 # Scripts
-alias init-project="sh $SCRIPTS/init-project.sh"
 alias download="sh $SCRIPTS/download.sh"
 alias volume="sh $SCRIPTS/volume.sh"
-alias dolar="python3 $SCRIPTS/dolar.py"
-alias myip="python3 $SCRIPTS/ip.py"
-alias kled="sh $SCRIPTS/change_led.sh"
-alias yen="python3 $SCRIPTS/yen.py"
-alias hkd="python3 $SCRIPTS/hkd.py"
-alias cny="python3 $SCRIPTS/cny.py"
-alias currencies="dolar ;and yen ;and hkd ;and cny"
 alias pall="sh $SCRIPTS/pull_everything.sh"
-
-# ENV variables
-export PROJECTS=$HOME/Projects
-export EDITOR=emacs
-export GIT_EDITOR=vim
-export TERM=st
 
 function fish_prompt
     set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
