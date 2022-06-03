295e4916d326a4606f7dacb5e324c8c56c16172e
Edison Neto <edison.aguiar.neto@gmail.com>
2022-06-03 01:13:17 -0300

    configure rustic to format file on save

--- .emacs.d/lisp/rust-config.el
+++ .emacs.d/lisp/rust-config.el
@@ -6,7 +6,8 @@
 (use-package rustic
   :ensure t
   :config
-  (setq lsp-rust-server 'rust-analyzer))
+  (setq lsp-rust-server 'rust-analyzer)
+  (setq rustic-format-on-save t))
 
 (provide 'rust-config)
 
