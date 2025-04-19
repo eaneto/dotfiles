77602c96f1029e807ef59cdc1f3a55e7fdc2a2f8
Edison Neto <eaneto@posteo.com>
2025-04-19 13:49:26 -0300

    update insitallation script
    creating links to directories instead of individual files where
    possible.

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 # Emacs
-mkdir -p ~/.emacs.d/lisp
-mkdir -p ~/.emacs.d/lisp/cql-mode
-mkdir -p ~/.emacs.d/snippets/org-mode
-
 ln -s ~/projects/dotfiles/.emacs.d/ufp_logo.txt ~/.emacs.d
 ln -s ~/projects/dotfiles/.emacs.d/init.el ~/.emacs.d
 ln -s ~/projects/dotfiles/.emacs.d/main.el ~/.emacs.d
@@ -12,17 +8,8 @@ ln -s ~/projects/dotfiles/.emacs.d/packages.el ~/.emacs.d
 ln -s ~/projects/dotfiles/.emacs.d/linux-style.el ~/.emacs.d
 ln -s ~/projects/dotfiles/.emacs.d/pg-style.el ~/.emacs.d
 ln -s ~/projects/dotfiles/.emacs.d/customizations.el ~/.emacs.d
-ln -s ~/projects/dotfiles/.emacs.d/lisp/rust-config.el ~/.emacs.d/lisp
-ln -s ~/projects/dotfiles/.emacs.d/lisp/lsp-mode-c.el ~/.emacs.d/lisp
-ln -s ~/projects/dotfiles/.emacs.d/lisp/python-config.el ~/.emacs.d/lisp
-ln -s ~/projects/dotfiles/.emacs.d/lisp/evil-config.el ~/.emacs.d/lisp
-ln -s ~/projects/dotfiles/.emacs.d/lisp/custom-modes-config.el ~/.emacs.d/lisp
-ln -s ~/projects/dotfiles/.emacs.d/lisp/cql-mode/cql-mode.el ~/.emacs.d/lisp/cql-mode
-ln -s ~/projects/dotfiles/.emacs.d/snippets ~/.emacs.d/snippets
-
-## Snippets
-ln -s ~/projects/dotfiles/.emacs.d/snippets/org-mode/begcm ~/.emacs.d/snippets/org-mode
-ln -s ~/projects/dotfiles/.emacs.d/snippets/org-mode/begsrc ~/.emacs.d/snippets/org-mode
+ln -s ~/projects/dotfiles/.emacs.d/lisp ~/.emacs.d/
+ln -s ~/projects/dotfiles/.emacs.d/snippets/ ~/.emacs.d/
 
 # Zsh
 ln -s ~/projects/dotfiles/.zshrc ~/
